1cc6101dad514addb4e233cf7683ed831d8b39a0
benjamin <benjaminyakobi@gmail.com>
2020-05-04 16:48:15 +0300

    simple image & Garbage.txt file have been added

--- venv/Steganography.py
+++ venv/Steganography.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ec9f4bb50d40847ea8185b07ac6a0c95fa35ff066f9e8e8ebc1ce258479b436
-size 3312
+oid sha256:ce78316be2dbbab54ef88e8c1d9f10749c902ebb7429113d3f75a20cc9e594dc
+size 3450
