92b300e53b86b8ad0c5fe87cee987e0f752c313c
benjamin <benjaminyakobi@gmail.com>
2020-05-21 17:19:26 +0300

    Image.open to PIL.Image.open fix

--- venv/Steganography.py
+++ venv/Steganography.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d86c27316d6543bd1f53cb92816e7f63037cf1e3697a09e9a04afe737c7725a
-size 4537
+oid sha256:4a74f139d2074cdc0af6a4d906ba14f182c6e9100723df5151b042b51b33f280
+size 4673
