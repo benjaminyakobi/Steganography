084b9c59e2f1574794b966d006e6599d6f909da6
benjamin <benjaminyakobi@gmail.com>
2020-05-22 17:23:13 +0300

    little fixes to GUI

--- venv/Steganography.py
+++ venv/Steganography.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7629c6861f1160f6d0798fba6727490de73d8ee1c04530b2304c296a35b3e225
-size 6489
+oid sha256:4892b7919cbfd26e52fa74a96eb6f5fa91c815e16025b2d72b4e30a4215e6a29
+size 4735
