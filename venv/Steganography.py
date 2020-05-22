8ab09fe4bee6ccf04fd22d18ac0ff3e8b444860f
benjamin <benjaminyakobi@gmail.com>
2020-05-22 16:02:19 +0300

    Upgarding GUI - encode & decode basically working

--- venv/Steganography.py
+++ venv/Steganography.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fcf7dd4149b8a7beb3fde956ada03115d90e875a2a988bbc0871de1b8bc17c8
-size 5019
+oid sha256:7629c6861f1160f6d0798fba6727490de73d8ee1c04530b2304c296a35b3e225
+size 6489
