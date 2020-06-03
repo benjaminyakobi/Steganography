33305c2f6fa27afb78bcd9e99dc76d8762e684a3
benjamin <benjaminyakobi@gmail.com>
2020-06-04 02:06:52 +0300

    Update: New GUI structure, still need more work to do

--- venv/Driver.py
+++ venv/Driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2381add2bb6b057e58f411b1aad7a404de81d0fcc888a7e323b245b3d94cd4a
-size 1496
+oid sha256:5ccacaeacc5ae327470954a2cd19a4b59fc8d7aa4a92b65617cc8b40cc5ab232
+size 1503
---
+++ venv/GUI.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea27f0cd95fda409ac218ffebcc6d64761ba402bb485c0dd70f9e7135cda0060
+size 2463
