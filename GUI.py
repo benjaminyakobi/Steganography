22b28dea85f4ca32c1d9992033823ea5267f3f7d
benjamin <benjaminyakobi@gmail.com>
2020-06-08 15:41:52 +0300

    updated GUI - little fix button

--- GUI.py
+++ GUI.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6bdeab646c08a3deec27baa8926dce454a0f84ce3671da61d0fa874dfc1bd6d
-size 5180
+oid sha256:4fd4799709dc3045ce401793f3f75bec81168384e0339ad03892a6e91d2df73c
+size 5334
